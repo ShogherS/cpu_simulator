261891817d3c9ac5418ca37b741ebd9ff0d8502e
Shoghshoghan <108759140+ShogherS@users.noreply.github.com>
2023-09-11 19:29:12 +0400

    Update instructions.h

--- instructions.h
+++ instructions.h
@@ -4,8 +4,8 @@
 #include <map>
 #include <functional>
 
-struct Instructions
-{
+struct Instructions					//an Instructions struct saves all the instructions 
+{							//it has ALU data member that saves arithmetic and logic instructions
 	std::map<std::string, std::function<void(int&, int)>> ALU{
 		{"MOV", [](int& a, int b){ a = b; }},
 		{"ADD", [](int& a, int b){ a += b; }},  
@@ -15,7 +15,7 @@ struct Instructions
 		{"AND", [](int& a, int b){ a &= b; }},
 		{"OR", [](int& a, int b){ a |= b; }},
 		{"NOR", [](int& a, int b){ a ^= b; }}
-	};
+	};						//CMP data member saves a lambda function that performs comparison
 	std::function<void(int, int, int&)> CMP = [](int r1, int r2, int& flag){ 
 			if (r1 == r2){
 				flag = 0;
@@ -24,7 +24,7 @@ struct Instructions
 			} else {
 				flag = 2;
 			}
-		};
+		};					//JMP data structure saves lambda functions that preforms jumps
 	std::map<std::string, std::function<void(int, int&, int)>> JMP{
 		{"JMP", [](int lable, int& pc, int fake){pc = lable;} },
 		{"JG", [](int lable, int& pc, int flag){flag == 1 ? pc = lable : pc;} },
